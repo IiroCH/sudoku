5e69136b06868185b940668f8bd2f6e9dfe0ec2c
Iiro Huhta <iiro.huhta@tuni.fi>
2025-05-10 00:00:33 +0300

    Fixed error with the sudoku board, added function for getting all cell values

--- game.js
+++ game.js
@@ -1,15 +1,32 @@
+function getCellValues() {
+    const board = document.getElementById("game-board");
+
+    let values = [];
+    for (let supercell of board.childNodes) {
+        for (let cell of supercell.childNodes) {
+            values.push(cell.innerText);
+        }
+    }
+
+    return values;
+};
+
+
 const init = () => {
     const board = document.getElementById("game-board");
 
     for (let i = 0; i < 9; i++) {
         const supercell = document.createElement("div");
-        supercell.classList.push("supercell");
+        supercell.classList.add("supercell");
 
-        const cell = document.createElement("div");
-        cell.classList.push("cell");
-        cell.innerText = "X";
+        for (let j = 0; j < 9; j++) {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            cell.innerText = "X";
 
-        supercell.appendChild(cell);
+            supercell.appendChild(cell);
+        }
+        
         board.appendChild(supercell);
     }
 };
